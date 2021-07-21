c7b4e2847861477c9b419f06f8708ecba5abf2f1
Filipe <andradefilipe2002@gmail.com>
2021-07-21 20:30:13 -0300

    Arruma o lint

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-//login
+// login
 
-//pega o botão do login e adiciona o listener:
+// pega o botão do login e adiciona o listener:
 
-let botaoLogin = document.querySelector("#botaoLogin");
-botaoLogin.addEventListener('click', login);
+const botaoLogin = document.querySelector('#botaoLogin');
 
 function login() {
- 
-    //salva os valores dos inputs:
-    let usuario = document.querySelector("#inputEmail").value;
-    let senha = document.querySelector("#inputSenha").value;
+  // salva os valores dos inputs:
+  const usuario = document.querySelector('#inputEmail').value;
+  const senha = document.querySelector('#inputSenha').value;
 
-    //compara com o login autorizado e retorna mensagem:
+  // compara com o login autorizado e retorna mensagem:
 
-    if((usuario === "tryber@teste.com") && (senha === "123456")) {
-        alert('Olá, Tryber!')
-    }
-    else {
-        alert('Login ou senha inválidos.')
-    }
+  if ((usuario === 'tryber@teste.com') && (senha === '123456')) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
 }
+botaoLogin.addEventListener('click', login);
